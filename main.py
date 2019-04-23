e012fc70023d0c50b7975fdecf65e12d6324d961
Didier Garcia <dgarcia@softwaresushi.com>
2019-04-23 00:02:26 -0400

    Update main.py to use the curses library so that we have more control over the screen.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 import random
 import time
 import os
+import curses
 
 questionsAsked = 0
 correctAnswers = 0
@@ -15,10 +16,6 @@ correctAnswers = 0
 # Empty Dictionary and Word List
 wordDefs = {}
 wordList = []
-
-def clearScreen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def getRandomWordDefs():
     tupleList = []
@@ -54,112 +51,127 @@ def getDistinctRandoms( limit, numberOfRandoms ):
 
     return distinctRandoms
 
-def getColorCode( color ):
-    colorCode = None
 
-    if color == "green":
-        colorCode = "\033[92m"
-    elif color == "red":
-        colorCode = "\033[91m"
-    elif color == "blue":
-        colorCode = "\033[94m"
-    elif color == "yellow":
-        colorCode = "\033[93m"
+def main(stdscr):
 
-    return colorCode
+  global questionsAsked
+  global correctAnswers
 
-def getColorCodeEnd():
-    return "\033[0m"
+  # Colors
+  curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+  curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+  curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
-def printColor( msg, color ):
+  # Load the definitions file and populate the dictionary
+  defsFilePointer = open("defs.txt", "r")
+  wordCounter = 0
 
-    start_color = getColorCode(color)
+  for line in defsFilePointer:
 
-    if start_color:
-        print(start_color + msg + getColorCodeEnd())
-    else:
-        print(msg)
+      word, definition = line.rstrip().split(":", 2)
+      # print("#{} word: {}, definition: {}".format(wordCounter, word, definition))
 
-# Load the definitions file and populate the dictionary
-defsFilePointer = open("defs.txt", "r")
-wordCounter = 0
+      wordDefs[word] = definition
+      wordList.append(word)
 
-for line in defsFilePointer:
+      wordCounter += 1
 
-    word, definition = line.rstrip().split(":", 2)
-    # print("#{} word: {}, definition: {}".format(wordCounter, word, definition))
+  defsFilePointer.close()
 
-    wordDefs[word] = definition
-    wordList.append(word)
+  stdscr.clear()
+  
+  start_time = time.time()
+  # Start the Game loop
+  while True:
+      # Pick the words for this challenge
+      currentWordDefs = getRandomWordDefs()
 
-    wordCounter += 1
+      # Choose the correct word/definition
+      correctChoice = random.randint(0, 3)
 
-defsFilePointer.close()
+      # Print the words
 
-clearScreen()
-print("\n\n")
+      line_num = 0
+      # Show the correct definition
+      stdscr.addstr(line_num,0, "> {}".format(currentWordDefs[correctChoice][2]))
+      
+      line_num += 5
+      stdscr.move(line_num,0)
 
-start_time = time.time()
-# Start the Game loop
-while True:
-    # Pick the words for this challenge
-    currentWordDefs = getRandomWordDefs()
+      # Show each word with a number choice
+      for i in range(4):
+          stdscr.addstr(line_num, 0, "{}. {}".format(i+1, currentWordDefs[i][1]))
+          line_num += 1
 
-    # Choose the correct word/definition
-    correctChoice = random.randint(0, 3)
+      stdscr.move(line_num, 0)
+      line_num += 1
 
-    # Print the words
+      questionsAsked += 1
 
-    # Show the correct definition
-    print("> {}".format(currentWordDefs[correctChoice][2]))
-    print()
+      # Get User's selection
+      stdscr.addstr(line_num, 0, '"quit" to exit!', curses.color_pair(3))
+      line_num += 1
+      stdscr.addstr(line_num, 0, '? ')
+      selection = stdscr.getkey()
 
-    # Show each word with a number choice
-    for i in range(4):
-        print("{}. {}".format(i+1, currentWordDefs[i][1]))
+      # Clear screen
+      stdscr.clear()
+      stdscr.refresh()
 
-    print()
-    questionsAsked += 1
+      # Handle User's selection
+      if selection == "quit" or selection == "q":
+          questionsAsked -= 1
+          break
+      elif selection not in ["1", "2", "3", "4"]:
+          # Ignore bad input
+          line_num += 2
+          stdscr.addstr(line_num, 0, "Wrong :(", curses.color_pair(2))
+          line_num +=1
+          stdscr.addstr(line_num, 0 ,"{} - {}".format(currentWordDefs[correctChoice][1], currentWordDefs[correctChoice][2]))
+      elif (int(selection) - 1) == correctChoice:
+          line_num += 2
+          stdscr.addstr(line_num, 0 , "Correct!!", curses.color_pair(1))
+          line_num += 1
+          stdscr.addstr(line_num, 0 ,"{} - {}".format(currentWordDefs[correctChoice][1], currentWordDefs[correctChoice][2]))
+          correctAnswers += 1
+      else:
+          line_num += 2
+          stdscr.addstr(line_num, 0, "Wrong :(", curses.color_pair(2))
+          line_num += 1
+          stdscr.addstr(line_num, 0, "{} - {}".format(currentWordDefs[correctChoice][1], currentWordDefs[correctChoice][2]))
 
-    # Get User's selection
-    printColor('"quit" to exit', "blue")
-    selection = input("? ")
+      line_num += 1
+      stdscr.move(line_num, 0)
 
-    # Clear screen
-    clearScreen()
+  # Finally print session stats
+  line_num = 0
+  elapsed_time = time.time() -start_time
+  line_num += 1
+  stdscr.addstr(line_num, 0, "=" * 25 + " Session Results " + "=" * 25)
+  if questionsAsked > 0:
+      line_num += 1
+      stdscr.addstr(line_num, 0, "Score: {}/{} - {:3.1f}%".format(correctAnswers, questionsAsked, correctAnswers/questionsAsked * 100))
 
-    # Handle User's selection
-    if selection == "quit":
-        questionsAsked -= 1
-        break
-    elif selection not in ["1", "2", "3", "4"]:
-        # Ignore bad input
-        printColor("Wrong :(", "red")
-        print("{} - {}".format(currentWordDefs[correctChoice][1], currentWordDefs[correctChoice][2]))
-    elif (int(selection) - 1) == correctChoice:
-        printColor("Correct!!", "green")
-        print()
-        correctAnswers += 1
-    else:
-        printColor("Wrong :(", "red")
-        print("{} - {}".format(currentWordDefs[correctChoice][1], currentWordDefs[correctChoice][2]))
+      # calculate time
+      # Hours?
+      hours = elapsed_time // 3600
+      minutes = (elapsed_time - (hours * 3600)) // 60
+      seconds = (elapsed_time - (hours * 3600) - (minutes * 60)) // 1
 
-    print()
+      line_num += 1
+      stdscr.addstr(line_num, 0, "Time: {:2.0f} Hrs {:2.0f} Mins {:2.0f} Secs".format(hours, minutes, seconds))
+  else:
+      line_num += 1
+      stdscr.addstr(line_num, 0, "Nothing to see here")
 
-# Finally print session stats
-elapsed_time = time.time() -start_time
-print("=" * 25 + " Session Results " + "=" * 25)
-if questionsAsked > 0:
-    print("Score: {}/{} - {:3.1f}%".format(correctAnswers, questionsAsked, correctAnswers/questionsAsked * 100))
+  line_num += 1
+  stdscr.addstr(line_num, 0, "=" * 67)
+  line_num += 1
+  stdscr.addstr(line_num, 0, "Press any key...")
 
-    # calculate time
-    # Hours?
-    hours = elapsed_time // 3600
-    minutes = (elapsed_time - (hours * 3600)) // 60
-    seconds = (elapsed_time - (hours * 3600) - (minutes * 60)) // 1
-    print("Time: {:2.0f} Hrs {:2.0f} Mins {:2.0f} Secs".format(hours, minutes, seconds))
-else:
-    print("Nothing to see here")
-print("=" * 67)
-print("\n")
+  # Wait for the user to press any key
+  stdscr.getkey()
 
+# Start a curses wrapped session.
+if __name__ == '__main__':
+  curses.wrapper(main)
